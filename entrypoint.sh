2203121676006b908a6dc42b75b461e234c721f4
Gerardo De La Cruz <netoxas3107@gmail.com>
2026-01-23 17:41:23 +0100

    fix: update project config to deploy on railway

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Set default PORT if not provided
+PORT=${PORT:-80}
+export PORT
+
 # Replace ${PORT} placeholder in the nginx config template and write to tmp
 envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /tmp/default.conf
 exec nginx -g 'daemon off;' -c /tmp/nginx.conf
